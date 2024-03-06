35cb370229ca7bb8e15845a911f3484c0cfdb8c9
HoangPhi21 <asbibi01022004@gmail.com>
2024-03-06 12:57:55 +0700

    first

---
+++ thu.js
@@ -0,0 +1,4 @@
+var tien = 100000;
+var ten = prompt("Bạn tên gì") || "Quý khách";
+var str = "Chào" + ten + "Tặng bạn" + tien;
+alert(str);
---
+++ thu.ts
@@ -0,0 +1,4 @@
+let tien : number = 100000;
+let ten : string = prompt("Bạn tên gì") || "Quý khách";
+let str = "Chào" + ten + "Tặng bạn" + tien ;
+alert(str);
